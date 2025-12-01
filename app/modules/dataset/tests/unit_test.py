db7752508b25a16ea250c238a22e83d90eabcbdb
Francisco Miguel Jiménez Morales <frajimmor2@alum.us.es>
2025-12-01 19:06:36 +0100

    test: download count unit test #58

--- app/modules/dataset/tests/unit_test.py
+++ app/modules/dataset/tests/unit_test.py
@@ -1,6 +1,8 @@
 import pytest
+import re
+import uuid
 
-from app.modules.dataset.models import DataSet, DSMetaData
+from app.modules.dataset.models import DataSet, DSMetaData, DSViewRecord, DSDownloadRecord
 
 
 @pytest.fixture(scope="module")
@@ -8,12 +10,15 @@ def test_dataset_id(test_client):
     from app import db
 
     with test_client.application.app_context():
-        ds_meta = DSMetaData(title="Test Dataset", description="This is a test dataset", publication_type="NONE")
+        ds_meta = DSMetaData(title="Test Dataset", description="This is a test dataset", publication_type="NONE", dataset_doi="122345/test_ds", tags="tag1,tag2")
         db.session.add(ds_meta)
         db.session.commit()
 
         dataset = DataSet(user_id=1, ds_meta_data_id=ds_meta.id)
         db.session.add(dataset)
+
+        view_record = DSViewRecord(dataset_id=dataset.id, user_id=1, view_cookie=str(uuid.uuid4()))
+        db.session.add(view_record)
         db.session.commit()
 
         dataset_id = dataset.id
@@ -22,6 +27,8 @@ def test_dataset_id(test_client):
 
     # Cleanup
     with test_client.application.app_context():
+        DSViewRecord.query.filter_by(dataset_id=dataset.id).delete()
+        DSDownloadRecord.query.filter_by(dataset_id=dataset.id).delete()
         db.session.delete(dataset)
         db.session.delete(ds_meta)
         db.session.commit()
@@ -39,3 +46,34 @@ def test_dataset_badge_html(test_client, test_dataset_id):
     assert response.status_code == 200
     assert b"<img" in response.data
     assert b"Test_Dataset" in response.data
+
+
+def test_download_count_present_in_body(test_client):
+    response = test_client.get(f"/doi/122345/test_ds/")
+    assert response.status_code == 200
+    assert b"Downloads" in response.data
+
+def test_download_count_feat(test_client, test_dataset_id):
+    response = test_client.get(f"/doi/122345/test_ds/")
+    assert response.status_code == 200
+    assert b"Downloads" in response.data
+    
+    html = response.data.decode("utf-8")
+    init_downloads = get_downloads_from_html(html)
+    
+    download = test_client.get(f"/dataset/download/1")
+    response2 = test_client.get(f"/doi/122345/test_ds/")
+    html2 = response2.data.decode("utf-8")
+    new_downloads = get_downloads_from_html(html2)
+    assert new_downloads != init_downloads
+    assert new_downloads == (init_downloads+1)
+
+    
+
+def get_downloads_from_html(html):
+    # Busca la celda <td>Downloads</td> seguida de otra celda <td>...</td>
+    patron = r"<td>\s*Downloads\s*</td>\s*<td>\s*(\d+)\s*</td>"
+
+    match = re.search(patron, html, re.IGNORECASE)
+    downloads_value = int(match.group(1))
+    return downloads_value
