6b18057181e0cda3ea05ff58e5ad05393abdd938
Curro <frajimmor2@alum.us.es>
2025-12-02 23:38:55 +0100

    style: linter refactor unified

--- app/modules/dataset/tests/test_selenium.py
+++ app/modules/dataset/tests/test_selenium.py
@@ -1,10 +1,8 @@
-import time
 import pytest
-
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 from core.environment.host import get_host_for_selenium_testing
-from selenium.webdriver.common.by import By
 from core.selenium.common import close_driver, initialize_driver
 
 
@@ -21,30 +19,30 @@ def host():
 
 
 def test_badge(driver, host):
-        driver.get(host)
-        driver.set_window_size(886, 969)
-        driver.find_element(By.LINK_TEXT, "Login").click()
-        driver.find_element(By.ID, "email").click()
-        driver.find_element(By.ID, "email").send_keys("user1@example.com")
-        driver.find_element(By.ID, "password").send_keys("1234")
-        driver.find_element(By.ID, "submit").click()
-        driver.find_element(By.LINK_TEXT, "Sample dataset 4").click()
-        driver.find_element(By.CSS_SELECTOR, ".card:nth-child(1) .btn:nth-child(4)").click()
-        driver.find_element(By.CSS_SELECTOR, ".btn:nth-child(5)").click()
-        driver.find_element(By.LINK_TEXT, "Doe, John").click()
-        driver.find_element(By.LINK_TEXT, "Log out").click()
+    driver.get(host)
+    driver.set_window_size(886, 969)
+    driver.find_element(By.LINK_TEXT, "Login").click()
+    driver.find_element(By.ID, "email").click()
+    driver.find_element(By.ID, "email").send_keys("user1@example.com")
+    driver.find_element(By.ID, "password").send_keys("1234")
+    driver.find_element(By.ID, "submit").click()
+    driver.find_element(By.LINK_TEXT, "Sample dataset 4").click()
+    driver.find_element(By.CSS_SELECTOR, ".card:nth-child(1) .btn:nth-child(4)").click()
+    driver.find_element(By.CSS_SELECTOR, ".btn:nth-child(5)").click()
+    driver.find_element(By.LINK_TEXT, "Doe, John").click()
+    driver.find_element(By.LINK_TEXT, "Log out").click()
 
 
 def test_download_count(driver, host):
-        driver.get(host)
-        driver.set_window_size(592, 734)
-        driver.find_element(By.CSS_SELECTOR, ".card:nth-child(1) .row > .badge").click()
-        driver.find_element(By.LINK_TEXT, "Download (84 bytes)").click()
-        driver.find_element(By.CSS_SELECTOR, ".card:nth-child(1) .row > .badge").click()
-        driver.find_element(By.CSS_SELECTOR, ".sidebar-toggle").click()
-        driver.find_element(By.CSS_SELECTOR, ".card:nth-child(1) .btn:nth-child(2) > .feather").click()
-        driver.find_element(By.LINK_TEXT, "Sample dataset 4").click()
-        driver.find_element(By.LINK_TEXT, "Download all (84 bytes)").click() 
+    driver.get(host)
+    driver.set_window_size(592, 734)
+    driver.find_element(By.CSS_SELECTOR, ".card:nth-child(1) .row > .badge").click()
+    driver.find_element(By.LINK_TEXT, "Download (84 bytes)").click()
+    driver.find_element(By.CSS_SELECTOR, ".card:nth-child(1) .row > .badge").click()
+    driver.find_element(By.CSS_SELECTOR, ".sidebar-toggle").click()
+    driver.find_element(By.CSS_SELECTOR, ".card:nth-child(1) .btn:nth-child(2) > .feather").click()
+    driver.find_element(By.LINK_TEXT, "Sample dataset 4").click()
+    driver.find_element(By.LINK_TEXT, "Download all (84 bytes)").click()
 
 
 def test_index():
@@ -61,7 +59,6 @@ def test_index():
         try:
             test_badge(driver, host)
             test_download_count(driver, host)
-            
 
         except NoSuchElementException:
             raise AssertionError("Test failed!")
